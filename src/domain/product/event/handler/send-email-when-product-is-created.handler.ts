ec58744e605165f067022de26832f96f35454037
Jose Motta <josemotta@bampli.com>
2022-08-05 12:20:13 -0300

    registrando nosso primeiro evento

--- src/domain/product/event/handler/send-email-when-product-is-created.handler.ts
+++ src/domain/product/event/handler/send-email-when-product-is-created.handler.ts
@@ -5,6 +5,6 @@ export default class SendEmailWhenProductIsCreatedHandler
   implements EventHandlerInterface<ProductCreatedEvent>
 {
   handle(event: ProductCreatedEvent): void {
-    //console.log(`Sending email to .....`); 
+    console.log(`Sending email to .....`); 
   }
 }
